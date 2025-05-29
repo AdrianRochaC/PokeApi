89e98a48dbc4e760d4468dbfe400f69f23c2bc5d
AdrianRocha <130408696+AdrianRochaC@users.noreply.github.com>
2025-05-29 03:33:57 +0000

    apk ""

--- src/supabase.jsx
+++
@@ -1,4 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-const supabaseUrl = 'https://dnbvhkfpkxgamevejusr.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRuYnZoa2Zwa3hnYW1ldmVqdXNyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc0MTg1ODQsImV4cCI6MjA2Mjk5NDU4NH0.fC-DRdLrE65U1Ecqsztl1Y9affQhnWBQdrqKqqxGtZY';
-export const supabase = createClient(supabaseUrl, supabaseKey);
